bdb4cab76ed5408d6efaabee23a4050f7f56bb47
33357 <im33357dr@gmail.com>
2024-06-30 11:41:06 +0800

    add FishingScientist

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -13,6 +13,11 @@
 <script lang="ts" setup>
 const activities = [
     {
+        content: '警惕科学家钓鱼合约',
+        link: 'https://github.com/33357/smartcontract-apps/blob/main/Event/FishingScientist.md',
+        timestamp: '2024-6-30',
+    },
+    {
         content: '反闪电贷协议 EIP7690',
         link: 'https://github.com/33357/smartcontract-apps/blob/main/EIPS/eip7690.md',
         timestamp: '2024-6-23',
